26c90f875fe99280698ab63fd232796f97bcd430
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2025-08-01 03:23:22 +0600

    extra comment removed

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,33 +1,18 @@
 import commands from '../commands/commandsArray.js';
 
 const commandMap = new Map(commands.map(cmd => [cmd.name, cmd]));
-// commandMap.set('list', {
-//   name: 'list',
-//   description: 'List all commands',
-//   execute: async (interaction) => {
-//     const commandList = commands
-//       .map(cmd => `/${cmd.name} - ${cmd.description}`)
-//       .join('\n');
-//     await interaction.reply(`Available commands:\n${commandList}`);
-//   }
-// });
 
 export default async function (interaction) {
-  // console.log(interaction);
-  console.log(commandMap);
-
-
   if (!interaction.isCommand()) return;
-  // console.log(interaction.commandName);
+
   if (interaction.commandName === 'list') {
-    console.log(`Executing command: ${interaction.commandName}`);
     const command = {
       name: 'list',
       description: 'Replies with ping pong!!',
       execute: async (interaction) => {
         const commandList = commands
           .map(cmd => {
-            if (cmd.name !== 'list') return `/${cmd.name} - ${cmd.description}` 
+            if (cmd.name !== 'list') return `/${cmd.name} - ${cmd.description}`
           })
           .join('\n');
         await interaction.reply(`Available commands:\n${commandList}`);
@@ -38,11 +23,7 @@ export default async function (interaction) {
   }
 
   const command = commandMap.get(interaction.commandName);
-  console.log(command);
-
   if (command) {
-    // console.log(command);
     await command.execute(interaction);
-
   }
 }
